1ccc4669cc9559804cce96b51b7f9234ee5ede3e
pamelakelly0023 <pamela.kelly95@gmail.com>
2022-03-20 20:49:50 -0300

    Adiciona funcionalidade de publicar e receber mensagem

---
+++ Publish/Program.cs
@@ -0,0 +1,47 @@
+﻿using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+using Grpc.Core;
+
+public class Pub
+{
+    string projectId = "name-project-ig";
+    string topicId= "my-topic";
+    string messageText = "Sua menssagem aqui";
+
+    public async Task PublishMessageAsync()
+    {
+        try{
+            TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
+            PublisherClient publisher = await PublisherClient.CreateAsync(topicName);
+
+            var pubsubMessage = new PubsubMessage
+            {
+                
+                Data = ByteString.CopyFromUtf8(messageText),
+                
+                Attributes =
+                {
+                    { "year", "2022" },
+                    { "author", "unknown" }
+                }
+            };
+            string message = await publisher.PublishAsync(pubsubMessage);
+
+            Console.WriteLine($"mensagem publicada: {message}");
+
+
+        }catch(RpcException e){
+           Console.WriteLine("Erro");
+        }
+
+    }
+    public static async Task Main()
+    {
+        var pub = new Pub();
+        await pub.PublishMessageAsync();
+    }
+    
+}  
+
+
+    
---
+++ Subscribe/Program.cs
@@ -0,0 +1,45 @@
+﻿using Google.Cloud.PubSub.V1;
+
+public class PullMessages
+{
+    string projectId = "name-project-id";
+    string subscriptionId = "my-sub";
+    bool acknowledge = false;
+    public async Task<List<PubsubMessage>> PullMessagesAsync()
+    {
+        
+            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+
+            SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+
+            var messages = new List<PubsubMessage>();
+             
+            Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+            {
+    
+                string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+                Console.WriteLine($"Mensagem {message.MessageId}: {text}");
+                if (message.Attributes != null)
+                {
+                    foreach (var attribute in message.Attributes)
+                    {
+                        Console.WriteLine($"{attribute.Key} = {attribute.Value}");
+                    }
+                }
+                return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+            });
+            
+            await Task.Delay(7000);
+            await subscriber.StopAsync(CancellationToken.None);
+            
+            await startTask;
+            return messages;
+            
+    }
+
+    public static async Task Main()
+    {
+        var sub = new PullMessages();
+        await sub.PullMessagesAsync();
+    }
+}
